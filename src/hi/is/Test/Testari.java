422d45130131ae2af9f96337b1a6e97be89fff2b
zurgur <harryfreyr@gmail.com>
2017-03-29 15:27:57 +0000

    test

---
+++ src/hi/is/Test/Testari.java
@@ -0,0 +1,13 @@
+package hi.is.Test;
+import org.junit.*;
+import static org.junit.Assert.*;
+/**
+ * Created by Alexander on 3/29/2017.
+ */
+public class Testari {
+
+    @Test
+    public void someTest() {
+        
+    }
+}
