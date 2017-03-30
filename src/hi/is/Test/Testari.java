4c81c45571d650765f814bf2e849310df06df09f
BrynjaPalina <brynjaps@outlook.com>
2017-03-30 09:55:14 +0000

    Merge branch 'master' of C:\Users\BrynjaPalina\IdeaProjects\ThrounHugbunadar with conflicts.

--- src/hi/is/Test/Testari.java
+++ src/hi/is/Test/Testari.java
@@ -1,22 +1,25 @@
-package hi.is.Test;
-import org.junit.*;
-import static org.junit.Assert.*;
+//package hi.is.Test;
+//import org.junit.*;
+
+/*import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.After;
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
+import static org.junit.Assert.*;*/
 /**
  * Created by Alexander on 3/29/2017.
  */
 public class Testari {
 
-    @Before
+    //@Before
     public void setUp() {
 
     }
 
-    @After
+    //@After
     public void tearDown(){
         
     }
 
-    @Test
+    //@Test
     public void someTest() {
 
     }
---
+++ src/hi/is/Test/hi/is/vidmot/LoginTest.java
@@ -0,0 +1,20 @@
+package hi.is.vidmot;
+
+//import org.junit.jupiter.api.Test;
+
+/*import static org.junit.jupiter.api.Assertions.*;*/
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by BrynjaPalina on 29/03/2017.
+ */
+class LoginTest {
+    @Test
+    void launch() {
+
+    }
+
+    
+
+}
